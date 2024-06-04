d75269cf22c4958bdac2229941db82b9b0ff4482
Rizki Darmawan <rizdar.contact@gmail.com>
2024-06-04 12:00:41 +0700

    init

---
+++ src/app/members/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function MembersPage() {
+  return (
+    <div>
+      <h3 className="text-3xl">This will be the MembersPage</h3>
+      <Link href="/">Go Back</Link>
+    </div>
+  );
+}
